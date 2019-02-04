bcdab36ec7fad04cac2028d6b956a22fb10eefa7
Ezra Raez <ezRAez10@gmail.com>
2019-02-04 09:11:26 -0800

    start models

--- patients/models.py
+++ patients/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class Village(models.Model):
+  name = models.CharField(max_length=200)
+
+class Patient(models.Model):
+    f_name = models.CharField(max_length=32)
+    l_name = models.CharField(max_length=32)
+    # village = relationshipToModel
+
+
+class Doctor(models.Model):
+    f_name = models.CharField(max_length=32)
+    l_name = models.CharField(max_length=32)
+    specialty = models.CharField(max_length=100)
